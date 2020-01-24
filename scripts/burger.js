680ef1210332fb3349ba78099401afba16b7ba85
oleggorkov <ole.gorkov@yandex.ru>
2020-01-24 11:45:13 +0300

    change structure

--- scripts/burger.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('.header-button').click(function(event) {
-        $('.header-button, .header-menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
